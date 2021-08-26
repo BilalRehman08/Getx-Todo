4deae9c361752e6b17eca75a1731ab716aa5f71c
BilalRehman08 <bilalrehman080808@gmail.com>
2021-08-26 15:43:11 -0700

    Login Screen UI

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:circadia/screens/loginScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    );
+  }
+}
