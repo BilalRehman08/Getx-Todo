93b28eacab03cf5a9255831e054f3fc43ce3bca1
BilalRehman08 <bilalrehman080808@gmail.com>
2021-08-27 07:22:42 -0700

    New Task Screen UI

--- lib/screens/Addtask.dart
+++ lib/screens/Addtask.dart
@@ -21,127 +21,129 @@ class _AddTaskState extends State<AddTask> {
           style: TextStyle(fontSize: 17),
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            color: Colors.white,
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                      hintStyle: TextStyle(
-                        fontSize: 13,
-                        color: Colors.grey[400],
-                      ),
-                      hintText: "Task Name",
-                      contentPadding: const EdgeInsets.all(15)),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      hintStyle:
-                          TextStyle(fontSize: 13, color: Colors.grey[400]),
-                      hintText: "Description",
-                      contentPadding: const EdgeInsets.all(15)),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      hintStyle:
-                          TextStyle(fontSize: 13, color: Colors.grey[400]),
-                      hintText: "Category",
-                      contentPadding: const EdgeInsets.all(15)),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      hintStyle:
-                          TextStyle(fontSize: 13, color: Colors.grey[400]),
-                      hintText: "Pick Date & Time",
-                      contentPadding: const EdgeInsets.all(15)),
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        hintStyle: TextStyle(
+                          fontSize: 13,
+                          color: Colors.grey[400],
+                        ),
+                        hintText: "Task Name",
+                        contentPadding: const EdgeInsets.all(15)),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        hintStyle:
+                            TextStyle(fontSize: 13, color: Colors.grey[400]),
+                        hintText: "Description",
+                        contentPadding: const EdgeInsets.all(15)),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        hintStyle:
+                            TextStyle(fontSize: 13, color: Colors.grey[400]),
+                        hintText: "Category",
+                        contentPadding: const EdgeInsets.all(15)),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        hintStyle:
+                            TextStyle(fontSize: 13, color: Colors.grey[400]),
+                        hintText: "Pick Date & Time",
+                        contentPadding: const EdgeInsets.all(15)),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Text("${_character}"),
-          ),
-          Container(
-            color: Colors.white,
-            height: 50,
-            child: Row(
-              children: [
-                Radio<Priority>(
-                  fillColor:
-                      MaterialStateColor.resolveWith((states) => Colors.red),
-                  activeColor: Colors.red,
-                  focusColor: Colors.red,
-                  value: Priority.red,
-                  groupValue: _character,
-                  onChanged: (Priority value) {
-                    setState(() {
-                      _character = value;
-                    });
-                  },
-                ),
-                Radio<Priority>(
-                  fillColor:
-                      MaterialStateColor.resolveWith((states) => Colors.orange),
-                  activeColor: Colors.orange,
-                  focusColor: Colors.orange,
-                  value: Priority.orange,
-                  groupValue: _character,
-                  onChanged: (Priority value) {
-                    setState(() {
-                      _character = value;
-                    });
-                  },
-                ),
-                Radio<Priority>(
-                  fillColor:
-                      MaterialStateColor.resolveWith((states) => Colors.blue),
-                  activeColor: Colors.blue,
-                  focusColor: Colors.blue,
-                  value: Priority.blue,
-                  groupValue: _character,
-                  onChanged: (Priority value) {
-                    setState(() {
-                      _character = value;
-                    });
-                  },
-                ),
-                Radio<Priority>(
-                  fillColor:
-                      MaterialStateColor.resolveWith((states) => Colors.green),
-                  activeColor: Colors.green,
-                  focusColor: Colors.green,
-                  value: Priority.green,
-                  groupValue: _character,
-                  onChanged: (Priority value) {
-                    setState(() {
-                      _character = value;
-                    });
-                  },
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text("${_character}"),
             ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.2,
-          ),
-          Container(
-              width: MediaQuery.of(context).size.width * 1,
-              height: 45,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xff6035D0),
-                ),
-                child: Text("ADD",
-                    style: TextStyle(
-                      color: Colors.white,
-                    )),
-              ))
-        ],
+            Container(
+              color: Colors.white,
+              height: 50,
+              child: Row(
+                children: [
+                  Radio<Priority>(
+                    fillColor:
+                        MaterialStateColor.resolveWith((states) => Colors.red),
+                    activeColor: Colors.red,
+                    focusColor: Colors.red,
+                    value: Priority.red,
+                    groupValue: _character,
+                    onChanged: (Priority value) {
+                      setState(() {
+                        _character = value;
+                      });
+                    },
+                  ),
+                  Radio<Priority>(
+                    fillColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.orange),
+                    activeColor: Colors.orange,
+                    focusColor: Colors.orange,
+                    value: Priority.orange,
+                    groupValue: _character,
+                    onChanged: (Priority value) {
+                      setState(() {
+                        _character = value;
+                      });
+                    },
+                  ),
+                  Radio<Priority>(
+                    fillColor:
+                        MaterialStateColor.resolveWith((states) => Colors.blue),
+                    activeColor: Colors.blue,
+                    focusColor: Colors.blue,
+                    value: Priority.blue,
+                    groupValue: _character,
+                    onChanged: (Priority value) {
+                      setState(() {
+                        _character = value;
+                      });
+                    },
+                  ),
+                  Radio<Priority>(
+                    fillColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.green),
+                    activeColor: Colors.green,
+                    focusColor: Colors.green,
+                    value: Priority.green,
+                    groupValue: _character,
+                    onChanged: (Priority value) {
+                      setState(() {
+                        _character = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.2,
+            ),
+            Container(
+                width: MediaQuery.of(context).size.width * 1,
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xff6035D0),
+                  ),
+                  child: Text("ADD",
+                      style: TextStyle(
+                        color: Colors.white,
+                      )),
+                ))
+          ],
+        ),
       ),
     );
   }
