a66c9d9dcf520175e9274d558e2f6e71c64044f9
BilalRehman08 <bilalrehman080808@gmail.com>
2021-08-27 13:48:01 -0700

    Logout User

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,10 @@
-import 'package:circadia/widgets/drawer.dart';
+import 'package:circadia/controllers/firebase_controller.dart';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class Home extends GetWidget<FirebaseController> {
+  final FirebaseController controller = FirebaseController();
   var scaffoldkey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
@@ -60,7 +58,76 @@ class _HomeState extends State<Home> {
             ),
             preferredSize: Size.fromHeight(60.0)),
       ),
-      drawer: drawerr(context),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.25,
+              color: Color(0xff6035D0),
+              child: DrawerHeader(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Center(
+                        child: CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.black,
+                      backgroundImage: AssetImage("assets/Logo.png"),
+                    )),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Ishaq Hassan",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  ListTile(
+                    contentPadding: const EdgeInsets.only(left: 15),
+                    leading: Icon(
+                      Icons.add,
+                      size: 40,
+                      color: Colors.black,
+                    ),
+                    title: Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        'New Task',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      controller.signout();
+                    },
+                    contentPadding: const EdgeInsets.only(left: 25),
+                    leading: Icon(
+                      Icons.logout,
+                      size: 28,
+                      color: Colors.black,
+                    ),
+                    title: Text(
+                      'Logout',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
       body: Center(child: Text("data")),
     );
   }
